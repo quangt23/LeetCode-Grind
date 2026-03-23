9e40122fe96a20899563d85cbf04ddacd32cd5af
Quang Tran <quangntran222@gmail.com>
2026-03-23 17:10:43 -0400

    added a whole bunch of stuff idk!

---
+++ solutions/744_find_smallest_character_greater_than_target.py
@@ -0,0 +1,21 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for letter in letters:
+            if letter > target:
+                return letter
+        return letters[0]
+    
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        if target >= letters[-1] or target < letters[0]:
+            return letters[0]
+        
+        low = 0
+        high = len(letters) - 1
+        while low <= high:
+            mid = (high + low) // 2
+            if target >= letters[mid]:
+                low = mid + 1
+            if target < letters[mid]:
+                high = mid - 1
+        return letters[low]
